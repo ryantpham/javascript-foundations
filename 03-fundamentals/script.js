b596fe4ca334192879754e26c06ddc440936ddec
Ryan Pham <rtpham10@gmail.com>
2024-02-10 20:23:52 -0800

    Set up files for 03-foundations

---
+++ 03-fundamentals/script.js
@@ -0,0 +1,22 @@
+
+// 1. Write a function called add7 that takes one number and returns that number + 7.
+
+function add7(num){
+    return num + 7;
+}
+
+// 2. Write a function called multiply that takes 2 numbers and returns their product.
+
+
+
+// 3. Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
+
+
+
+/* 4. Write a function called lastLetter that takes a string and returns the very last letter of that string:
+   - lastLetter("abcd") should return "d" */ 
+
+
+
+//Tests
+console.log(add7(10)) //expect 17
