74b8a1e095cc90c40fc5e2393a8469807f1b84be
Ryan Pham <rtpham10@gmail.com>
2024-02-25 14:53:19 -0800

    set a different attribute for href

--- dom-challenge-starting-files/index.js
+++ dom-challenge-starting-files/index.js
@@ -2,3 +2,5 @@ document.querySelector("ul").lastElementChild.innerHTML = "Ryan";
 
 document.querySelector(".list a").style.color = "red";
 document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("h1").classList.add("huge");
+//document.querySelector("a").setAttribute("href","www.bing.com")
