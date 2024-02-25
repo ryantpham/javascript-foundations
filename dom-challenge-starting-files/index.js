3dc49fa975de2dd17c1df0823c6895fb538d2138
Ryan Pham <rtpham10@gmail.com>
2024-02-25 13:36:01 -0800

    changed style of button using js

--- dom-challenge-starting-files/index.js
+++ dom-challenge-starting-files/index.js
@@ -1,3 +1,4 @@
 document.querySelector("ul").lastElementChild.innerHTML = "Ryan";
 
 document.querySelector(".list a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
